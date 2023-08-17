9dee3496a722a7991697cc0e208153adc5b40e97
Jonathan Edenström <jonathan@edenstrom.io>
2023-08-17 13:04:52 +0200

    add example with getSetCookie

---
+++ src/app/api/edgeGetSetCookie/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const runtime = "edge";
+
+export const GET = async (req: NextRequest) => {
+  const url = `${req.nextUrl.origin}/api/cookie`;
+  const sessionResponse = await fetch(url, {
+    credentials: "include",
+  });
+
+  const setCookies = (sessionResponse.headers as any).getSetCookie?.();
+
+  const response = NextResponse.json({
+    route: "????",
+  });
+
+  for (const cookie of setCookies) {
+    response.headers.append("set-cookie", cookie);
+  }
+
+  return response;
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,9 @@ export default function Home() {
       <div style={{ display: "flex", gap: 8 }}>
         <button onClick={fetchRoute("/api/node")}>Node API Route</button>
         <button onClick={fetchRoute("/api/edge")}>Edge API Route</button>
+        <button onClick={fetchRoute("/api/edgeGetSetCookie")}>
+          Edge API Route (.getSetCookie)
+        </button>
         <button onClick={fetchRoute("/api/parseEdge")}>
           Edge API Route with parsing
         </button>
